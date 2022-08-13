38bba61152c9926eabea5a058da92c29cdb2028a
nimat <niymet.p@ya.ru>
2022-08-13 21:53:32 +0300

    Завершение работы

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -16,4 +16,32 @@ Console.WriteLine(u);
 }
 */
 
+// Задача 13
+/*
+Console.WriteLine("Введите число");
+int i13 = Convert.ToInt32(Console.ReadLine());
+if (i13>99) 
+{
+    string str = Convert.ToString(i13);
+    char t = str[2];
+    Console.WriteLine(t);
+} else 
+{
+    Console.WriteLine("Число не трехзначное");
+}
+*/
 
+// Задача 15
+Console.WriteLine("Введите число");
+int l = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(l);
+if (l<1 || l>7)
+{
+    Console.WriteLine("Число не подходит");
+} else if (l>0 && l<6) 
+{
+    Console.WriteLine("Рабочий день");
+} else if (l>5 && l<8)
+{
+    Console.WriteLine("Выходной день");
+}
