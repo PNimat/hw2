5fec64a774fbd17dd97a6c61ac435dc8c715e271
nimat <niymet.p@ya.ru>
2022-08-13 20:16:07 +0300

    пошла работа

---
+++ hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10
+/*
+Console.WriteLine("Введите трехзначное число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i>=100 && i<=999)
+{
+
+int y = i%100;
+int u = y/10;
+Console.WriteLine(u);
+
+} else 
+{
+    Console.WriteLine("Число не трехзначное");
+}
+*/
+
+
